a961f5ee98173a1ee08ab28970499a78e309e39b
hfsaito <hiroshi.fs94@gmail.com>
2025-08-19 08:29:52 -0300

    changes after review

--- Assets/Scripts/SpawnArmy/Army.cs
+++ Assets/Scripts/SpawnArmy/Army.cs
@@ -7,8 +7,8 @@ public class Army : MonoBehaviour
     private static readonly int N_UNITS = 10000;
     private static readonly int UNITS_PER_ROW = Mathf.FloorToInt(Mathf.Sqrt(N_UNITS));
 
-    private static readonly Vector3 SPACING = new Vector3(2.12f, 2.86f, 0);
-    private static readonly Vector3 OFFSET = (- UNITS_PER_ROW / 2) * SPACING;
+    private static readonly Vector3 SPACING = new(2.12f, 0, 2.86f);
+    private static readonly Vector3 OFFSET = new((- UNITS_PER_ROW / 2) * 2.12f, -5, 5);
 
     private static readonly Quaternion DEFAULT_ROTATION = new(0, 0, 0, 0);
 
@@ -18,7 +18,7 @@ public class Army : MonoBehaviour
         {
             for(int i = 0; i < UNITS_PER_ROW; i++)
             {
-                Vector3 position = OFFSET + Vector3.Scale(new Vector3(i, j, 0), SPACING);
+                Vector3 position = OFFSET + Vector3.Scale(new Vector3(i, 0, j), SPACING);
                 Instantiate(unit, position, DEFAULT_ROTATION);
             }
         }
